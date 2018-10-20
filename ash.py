b1a73d99ed124f0a675c9a53a8e96302f704d25c
Ashutosh 64 <ashutoshrath64@gmail.com>
2018-10-20 22:19:18 +0530

    hi

---
+++ ash.py
@@ -0,0 +1,19 @@
+#print something
+print("hi i am ashutosh")
+#print concinate string
+print("hello"+"world")
+#user difine variables
+number=input('number')
+print(number)
+#print lets get started
+print("lets get started")
+s="acadview"
+course="python"
+fees=5000
+print("%s  %s  fees is %d" %(s,course,fees))
+# find the area of circle
+radius=float(input("insert radius"))
+pi=3.14
+area=pi*radius*radius
+print(area)
+
