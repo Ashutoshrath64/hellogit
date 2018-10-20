8ce7563cbc7cc9d0e77ad5ccd5342db6239c9190
Ashutosh 64 <ashutoshrath64@gmail.com>
2018-10-20 22:23:33 +0530

    ASSIGNMENT

---
+++ assignment.py
@@ -0,0 +1,39 @@
+bff=['vijaya','manthan','ashutosh','sidharth']
+lst=['google','apple','facebook','microsoft','tesla']
+rst=bff+lst
+print(bff)
+print(rst)
+lst.count("google")
+print(lst.count("google"))
+number=[1,2,3,4,5,6,7,8,9,10]
+odd=[]
+even=[]
+for x in number:
+    if x%2==0:
+        even.append(x)
+    else:
+        odd.append(x)
+
+print(odd)
+print(even)
+tuple=(1,2,3,4,5,6,7,8,9,10)
+b=tuple[::-1]
+print(b)
+print(max(tuple))
+print(min(tuple))
+str="vijay"
+print(str.upper())
+ash="771039"
+ash.isnumeric()
+print(ash.isnumeric())
+ash2="hello world"
+print(ash2.replace("world","riki"))
+
+
+
+
+
+
+
+
+
